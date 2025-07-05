12ecd6ecfb4923be1c2c34a84f82d5557a71a11f
Pavel Maksimov <vur21@yandex.ru>
2025-07-05 12:08:03 +0300

    refactor(log): split and enhance timer functions

--- project/utils/log.py
+++ project/utils/log.py
@@ -1,6 +1,7 @@
 import logging
 import time
 from contextlib import contextmanager
+from typing import Callable, Any, Generator
 
 
 @contextmanager
@@ -11,8 +12,32 @@ def logging_disabled(level="CRITICAL"):
 
 
 @contextmanager
-def timer(logger: logging.Logger, name: str = "", template: str = "{name}, {duration} sec."):
+def timer() -> Generator[Callable[[], float], Any, None]:
+    """
+    with timer() as get_elapsed:
+
+    time.sleep(1)
+    print(f"Time has passed: {get_elapsed()} sec.")
+
+    time.sleep(1)
+    print(f"Time has passed: {get_elapsed()} sec.")
+    """
+    begin = time.perf_counter()
+    elapsed = 0
+
+    def update_elapsed():
+        nonlocal elapsed
+        elapsed = round(time.perf_counter() - begin, 3)
+        return elapsed
+
+    yield update_elapsed
+
+    update_elapsed()
+
+
+@contextmanager
+def timer_log(logger: logging.Logger, name: str = "", template: str = "{name}, {duration} sec."):
     begin = time.perf_counter()
     yield
     message = template.format(name=name or logger.name, duration=round(time.perf_counter() - begin, 3))
-    logger.info(message)
+    logger.debug(message)
