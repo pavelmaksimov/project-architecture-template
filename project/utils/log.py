e13b79e638adc437fafd02c52affffc064088909
Pavel Maksimov <vur21@yandex.ru>
2025-10-17 13:14:22 +0300

    refactor(utils): simplify elapsed time calculation

--- project/utils/log.py
+++ project/utils/log.py
@@ -25,16 +25,7 @@ def timer() -> t.Generator[t.Callable[[], float], t.Any, None]:
     print(f"Time has passed: {get_elapsed()} sec.")
     """
     begin = time.perf_counter()
-    elapsed = 0
-
-    def update_elapsed():
-        nonlocal elapsed
-        elapsed = round(time.perf_counter() - begin, 3)
-        return elapsed
-
-    yield update_elapsed
-
-    update_elapsed()  # di: skip
+    yield lambda: round(time.perf_counter() - begin, 3)
 
 
 @contextmanager
