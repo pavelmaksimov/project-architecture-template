56b07d36d94f928d46614ad1ce9d7439aac99797
Pavel Maksimov <vur21@yandex.ru>
2025-12-01 14:50:53 +0300

    chore: disable di-linter

--- project/components/chat/use_cases.py
+++ project/components/chat/use_cases.py
@@ -1,8 +1,8 @@
 import typing as t
 
+from project.components.chat.enums import MessageTypeEnum
 from project.components.user.schemas import UserCacheSchema
 from project.datatypes import UserIdT, QuestionT, AnswerT
-from project.components.chat.enums import MessageTypeEnum
 from project.settings import Settings
 
 if t.TYPE_CHECKING:
@@ -26,20 +26,21 @@ class Chat:
         """
         Задать вопрос в чат. Вернет текст ответа от AI.
         """
-        self.repo.message.save_user_message(user_id, question)
+        with self.repo.transaction():
+            self.repo.message.save_user_message(user_id, question)
 
-        history_messages = self.repo.message.get_chat_history(
-            user_id=user_id,
-            limit=Settings().HISTORY_WINDOW,
-        )
+            history_messages = self.repo.message.get_chat_history(
+                user_id=user_id,
+                limit=Settings().HISTORY_WINDOW,
+            )
 
-        answer_text = self.chat_agent.generate_answer(question, history_messages)
+            answer_text = self.chat_agent.generate_answer(question, history_messages)
 
-        self.repo.message.save_ai_message(user_id, answer_text)
+            self.repo.message.save_ai_message(user_id, answer_text)
 
-        self.repo.user_cache.save(user_id, UserCacheSchema(user_id=user_id))
+            self.repo.user_cache.save(user_id, UserCacheSchema(user_id=user_id))
 
-        return answer_text
+            return answer_text
 
     def get_history(self, user_id: UserIdT, limit: int = 20) -> list[dict[str, QuestionT | AnswerT]]:
         """
