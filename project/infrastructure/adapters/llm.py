15f5af2dbe6b8e7d4d90950b567e52e4068852df
Pavel Maksimov <vur21@yandex.ru>
2025-12-02 16:41:41 +0300

    feat: add reranker client

--- project/infrastructure/adapters/llm.py
+++ project/infrastructure/adapters/llm.py
@@ -1,5 +1,6 @@
 from functools import cache
 
+import cohere
 from langchain_openai import ChatOpenAI
 from llm_common.clients.llm_http_client import LLMHttpClient
 from openai import AsyncClient
@@ -28,4 +29,25 @@ def llm_aclient() -> AsyncClient:
         api_key=Settings().LLM_API_KEY.get_secret_value(),
         base_url=Settings().LLM_MIDDLE_PROXY_URL if Settings().is_any_stand() else None,
         http_client=LLMHttpClient(),
+    )
+
+
+@cache
+def reranker_client() -> cohere.AsyncClient:
+    """
+    Client with prometheus monitoring.
+
+    Example usage:
+        await reranker_client().rerank(
+            model=Settings().RERANK_MODEL,
+            query="...",
+            documents=[...],
+            top_n=3,
+        )
+
+    """
+    return cohere.AsyncClient(
+        api_key=Settings().LLM_API_KEY.get_secret_value(),
+        base_url=Settings().LLM_MIDDLE_PROXY_URL if Settings().is_any_stand() else None,
+        httpx_client=LLMHttpClient(),
     )
